44065d00d956c7190902b7d6a7e78527efffe9e4
Rolv-Arild <rolv_arild@hotmail.com>
2020-12-12 00:30:32 +0100

    Add __init__.py import

---
+++ ballchasing/__init__.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# MIT License
+#
+# Copyright (c) 2020 Rolv-Arild Braaten
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""A library that provides a Python interface to the Ballchasing API."""
+from __future__ import absolute_import
+
+__author__       = 'Rolv-Arild Braaten'
+# __email__        = ''
+__copyright__    = 'Copyright (c) 2020 Rolv-Arild Braaten'
+__license__      = 'Apache License 2.0'
+__version__      = '0.1.2'
+__url__          = 'https://github.com/Rolv-Arild/python-ballchasing'
+__download_url__ = 'https://pypi.python.org/pypi/python-ballchasing'
+__description__  = 'A Python wrapper around the Ballchasing API'
+
+from .api import Api                        # noqa
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='python-ballchasing',
-    version='0.1',
+    version='0.1.2',
     author="Rolv-Arild Braaten",
     author_email="rolv_arild@hotmail.com",
     description="Python wrapper for the ballchasing.com API",
